9872026fb8203ff08d99a54ce035734bf2b739b6
Joshua Mostafa <micapam@gmail.com>
2014-03-04 17:12:17 +1100

    Fix bug on refresh views and triggers generator

--- lib/generators/multitenant/views_and_triggers/refresh_generator.rb
+++ lib/generators/multitenant/views_and_triggers/refresh_generator.rb
@@ -11,6 +11,13 @@ module Multitenant
       desc "drops all views and triggers and creates new ones based on configs"
 
       def generate_mysql_views
+        Multitenant::Mysql.configs.models.each do |m|
+
+          # 'column_names' are set on first call; we are accessing it for each
+          # model so the values are available after dropping the views - because
+          # it will no longer be possible to look up the columns for each view.
+          m.constantize.column_names
+        end
         Multitenant::Views::SQL::Drop.run
         Multitenant::Views::SQL::Create.run
       end
