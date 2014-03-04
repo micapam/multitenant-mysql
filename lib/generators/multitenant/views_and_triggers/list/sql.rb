cda0af337890a53b71dec72faefc102e6bcdddf5
Joshua Mostafa <micapam@gmail.com>
2014-03-04 13:24:37 +1100

    Use a module variable rather than a function so client code can set the value rather than having to monkey patch.

--- lib/generators/multitenant/views_and_triggers/list/sql.rb
+++ lib/generators/multitenant/views_and_triggers/list/sql.rb
@@ -3,9 +3,10 @@ module Multitenant
     VIEWS = "SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE TABLE_TYPE LIKE 'VIEW' AND TABLE_SCHEMA LIKE '#{Multitenant::Mysql::DB.configs['database']}';"
     TRIGGERS = "SELECT trigger_name FROM information_schema.TRIGGERS where trigger_schema = '#{Multitenant::Mysql::DB.configs['database']}';"
 
-    def tenant_sql_fragment
-      'SUBSTRING_INDEX(USER(), \'@\', 1)'
+    mattr_writer :tenant_sql_fragment
+
+    def self.tenant_sql_fragment
+      @@tenant_sql_fragment ||= 'SUBSTRING_INDEX(USER(), \'@\', 1)'
     end
-    module_function :tenant_sql_fragment
   end
 end
